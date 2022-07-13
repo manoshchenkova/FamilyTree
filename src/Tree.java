697ae69fbdf711119c8a98e4a0d2e4731fdaf62e
Marina Anoschenkova <chervyakova.marina@gmail.com>
2022-07-13 23:24:04 +0300

    Add gitignore

---
+++ src/Tree.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Tree {
+	
+	List<Person> people; 
+	List<Node> nodes;
+	
+	public void addPerson(Person personWeAdd) {
+//		personWeAdd.setFirstName();
+	}
+	
+}
